ac3b255dea67638902bfc890579c5e91d3b73085
amohan14 <ananya.mhty@gmail.com>
2020-08-27 00:56:07 -0400

    Update wp.sh
    Updated firewall code

--- wp.sh
+++ wp.sh
@@ -1,17 +1,22 @@
 #!/usr/bin/env bash
 
 # Name of the WordPress tarball
-tarball="wordpress-5.1.1.tar.gz"
+# tarball="wordpress-5.1.1.tar.gz"
 
 # Checks to see if User passed a variable from the command line
 # If they did not, sets the default password to Drawsap
 password=${1:-root}
 
-# Install and Start Apache Web Server
+# Install and Start Apache Web Server. Setting up the firewall to allow HTTP/HTTPs traffic(Port 80)
+)
 install_apache () {
   echo "Install and Start Apache Web Server"
   cd $HOME
   sudo yum install httpd -y
+  sudo yum install firewalld
+  sudo firewall-cmd --permanent --add-service=http
+  sudo firewall-cmd --permanent --add-service=https
+  sudo firewall-cmd --reload
   sudo systemctl start httpd.service
   sudo systemctl enable httpd.service
   echo "Finished"
@@ -23,7 +28,7 @@ install_mysql () {
   cd $HOME
   sudo yum install mariadb-server mariadb -y
   sudo systemctl start mariadb
-  # sudo mysql_secure_installation
+  # mysql_secure_installation
   mysql -u root -e "UPDATE mysql.user SET Password=PASSWORD('$password') WHERE User='root';"
   mysql -u root -e "DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');"
   mysql -u root -e "DELETE FROM mysql.user WHERE User='';"
@@ -41,17 +46,13 @@ install_php () {
   sudo yum install php php-mysql -y
   sudo systemctl restart httpd.service
   echo "Finished"
-  # Open Firewall for http/https traffic (also ensure portforwarding is done for port 80)
-  # sudo firewall-cmd --permanent --zone=public --add-service=http 
-  # sudo firewall-cmd --permanent --zone=public --add-service=https
-  # sudo firewall-cmd --reload
 }
 
 # Create MySQL Database and User for WordPress
 set_up_sql_user () {
   echo "Create MySQL Database and User for WordPress"
   cd $HOME
-  mysql -u root -p$password -e "CREATE DATABASE IF NOT EXISTS wordpress DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci;"
+  mysql -u root -p$password -e "CREATE DATABASE IF NOT EXISTS wordpress;"
   mysql -u root -p$password -e "CREATE USER IF NOT EXISTS wordpressuser@localhost IDENTIFIED BY 'password';"
   mysql -u root -p$password -e "GRANT ALL PRIVILEGES ON wordpress.* TO wordpressuser@localhost IDENTIFIED BY 'password';"
   mysql -u root -p$password -e "FLUSH PRIVILEGES;"
@@ -62,10 +63,11 @@ set_up_sql_user () {
 install_wordpress () {
   echo "Install WordPress"
   cd $HOME
-  sudo yum install php-curl php-gd -y
+  sudo yum install wget -y
+  sudo yum install php-gd -y
   sudo systemctl restart httpd
-  wget https://wordpress.org/$tarball
-  tar -xzf $tarball
+  wget http://wordpress.org/latest.tar.gz
+  tar -xzf latest.tar.gz
   sudo rsync -avP ~/wordpress/ /var/www/html/
   mkdir /var/www/html/wp-content/uploads
   sudo chown -R apache:apache /var/www/html/*
